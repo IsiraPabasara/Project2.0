eb8fe4e5c5c77ae3910895ee6ee84a542dddd4c2
IsiraPabasara <isirapabasara123@gmail.com>
2025-05-18 19:31:00 +0530

    Updated ReportView.java

--- Project2.0/src/main/java/view/ReportView.java
+++ Project2.0/src/main/java/view/ReportView.java
@@ -206,7 +206,8 @@ public class ReportView extends JFrame {
         loadData();
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SwingUtilities.invokeLater(ReportView::new);
-    }
+    }*/
+
 }
